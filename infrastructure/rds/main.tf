cd5a9a933f510e5ea7f1ce12bbc2126562926751
wiktor <wiktor.kycia@gmail.com>
2025-07-04 13:30:13 +0200

    add basic main.tf file

---
+++ infrastructure/rds/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "bed-occupancy-terraform-remote-state"
+    key    = "bed-occupancy/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = local.region
+}
