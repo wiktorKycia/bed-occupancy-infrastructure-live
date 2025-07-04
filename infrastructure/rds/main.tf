f7b6b4c2d72162f91b0612c373d4a69cfda14e9b
wiktor <wiktor.kycia@gmail.com>
2025-07-04 13:42:45 +0200

    copy code from github example

--- infrastructure/rds/main.tf
+++ infrastructure/rds/main.tf
@@ -16,3 +16,94 @@ terraform {
 provider "aws" {
   region = local.region
 }
+
+# automatic arn and account data detection
+data "aws_caller_identity" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+# local variables
+locals {
+  region = "eu-central-1"
+  name   = "bed-occupancy"
+
+  vpc_cidr = "10.0.0.0/16"
+
+  docker_images = [
+    "frontend",
+    "backend",
+    "faker",
+    "db"
+  ]
+
+  tags = {
+    Name       = local.name
+    Repository = "https://github.com/wiktorKycia/bed-occupancy-infrastructure-live/tree/basic-config-and-ecr"
+  }
+}
+
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = local.name
+
+  engine                   = "postgres"
+  engine_version           = "14"
+  engine_lifecycle_support = "open-source-rds-extended-support-disabled"
+  family                   = "postgres14" # DB parameter group
+  major_engine_version     = "14"         # DB option group
+  instance_class           = "db.t4g.large"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+
+  db_name  = "postgres"
+  username = "postgres"
+  port     = 5432
+
+  manage_master_user_password_rotation              = false
+  master_user_password_rotate_immediately           = false
+
+  multi_az               = true
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+  create_monitoring_role                = true
+  monitoring_interval                   = 60
+  monitoring_role_name                  = "example-monitoring-role-name"
+  monitoring_role_use_name_prefix       = true
+  monitoring_role_description           = "Description for monitoring role"
+
+  parameters = [
+    {
+      name  = "autovacuum"
+      value = 1
+    },
+    {
+      name  = "client_encoding"
+      value = "utf8"
+    }
+  ]
+
+  tags = local.tags
+  db_option_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_parameter_group_tags = {
+    "Sensitive" = "low"
+  }
+  cloudwatch_log_group_tags = {
+    "Sensitive" = "high"
+  }
+}
