1617010c600b68aab487999ce2719ae1c1d54ef1
Wiktor Kycia <102750427+wiktorKycia@users.noreply.github.com>
2025-07-08 12:44:21 +0200

    Basic folder structure and ECR config (#2)
    * create basic terraform template
    
    * create an ecr resource
    
    * add ecr as a module, create local and global variables, create outputs file
    
    * correct back the region in backend
    
    * add a comment for clarity
    
    * new folder structure
    
    * remove unused code
    
    * use variable for mutability
    
    * create one ecr module for each docker image
    
    * add faker to docker images
    
    * convert local variable docker_images to a list
    
    * add vpc terraform configuration
    
    * update the provider version in order to run vpc
    
    * add a human-readable name to the public subnet
    
    * moved azs to local
    
    * add basic main.tf file
    
    * Merge branch 'vpc'
    
    * copy code from github example
    
    * move some values to variables.tf file, copy and paste outputs.tf file from example on github
    
    * update the version and make the plan
    
    * add outputs.tf file and output the db subnet group variable
    
    * move some values to variables, define outputs
    
    * in vpc output database subnet group name, which is needed by db module in rds
    
    * adjust some spaces
    
    * remove non-existing key from outputs
    
    * remove .gitkeep
    
    * Revert "Merge branch 'correcting-vpc-rds'"
    
    This reverts commit b913ff085f1bae3b0ed070487a40a8d515c48d0f, reversing
    changes made to 19b46b27015b8fbf842ff0f9e1b27fc851f8be66.

---
+++ infrastructure/ecr/main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "bed-occupancy-terraform-remote-state"
+    key    = "bed-occupancy/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = local.region
+}
+
+# local variables
+locals {
+  region = "eu-central-1"
+  name   = "bed-occupancy"
+
+  docker_images = [
+    "frontend",
+    "backend",
+    "faker",
+    "db"
+  ]
+
+  tags = {
+    Name       = local.name
+    Repository = "https://github.com/wiktorKycia/bed-occupancy-infrastructure-live/tree/basic-config-and-ecr"
+  }
+}
+
+# automatic arn and account data detection
+data "aws_caller_identity" "current" {}
+
+module "ecr" {
+  source = "terraform-aws-modules/ecr/aws"
+
+  for_each = toset(local.docker_images)
+
+  repository_name = "${local.name}-${each.key}"
+
+  repository_read_write_access_arns = [data.aws_caller_identity.current.arn]
+  repository_lifecycle_policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1,
+        description  = "Keep last 10 images",
+        selection = {
+          tagStatus     = "any",
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+
+  repository_image_tag_mutability = var.repository_image_tag_mutability
+
+  repository_force_delete = var.repository_force_delete
+
+  tags = merge(local.tags, {
+    Terraform   = "true"
+    Environment = "dev"
+    ServiceName = each.key
+  })
+}
